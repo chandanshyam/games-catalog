704cadc45337b579f4351d81560aeb6d0776a81a
chandanshyam96 <91785613+chandanshyam96@users.noreply.github.com>
2024-05-30 19:36:15 -0600

    Dark Mode Implemented

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,10 @@
 import { HStack, Image, Text } from "@chakra-ui/react";
-import logo from "../assets/logo.png";
+import logo from "../assets/logo.webp";
 
 const NavBar = () => {
   return (
     <HStack>
-      <Image src="logo" boxSize={"60px"}></Image>
+      <Image src={logo} boxSize={"60px"}></Image>
       <Text>Nav Bar</Text>
     </HStack>
   );
